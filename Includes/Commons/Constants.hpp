fcb31e931959e7326e2d4aed0b71666ac95e0ebb
Chris Ohk <utilForever@gmail.com>
2018-12-17 13:38:40 +0900

    refactor: Change OPERATOR's type to std::vector<std::string>

--- Includes/Commons/Constants.hpp
+++ Includes/Commons/Constants.hpp
@@ -7,9 +7,9 @@
 
 namespace CubbyCalc
 {
-constexpr const char* OPERATORS[] = { "+",  "-",   "*",   "/",   "=", "^",
-                                      "u-", "sin", "cos", "tan", "(" };
-constexpr size_t OPERATORS_SIZE = sizeof(OPERATORS) / sizeof(const char*);
+const std::vector<std::string> OPERATORS = { "+",   "-",   "*",  "/",
+                                             "=",   "^",   "u-", "sin",
+                                             "cos", "tan", "(" };
 
 constexpr int INIT_INPUT_PRIORITY[] = { 4, 4, 5, 5, 1, 6, 7, 7, 7, 7, 8 };
 constexpr int INIT_STACK_PRIORITY[] = { 4, 4, 5, 5, 1, 6, 7, 7, 7, 7, 0 };
@@ -23,23 +23,23 @@ const std::string OPERAND_CHARS(ID_CHARS + DIGITS + '.');
 
 using PriorityTable = std::map<std::string, int>;
 
-inline PriorityTable InitPriorityTable(const char* const opList[],
-                                       const size_t opSize, const int* priority)
+inline PriorityTable InitPriorityTable(const std::vector<std::string> opList,
+                                       const int* priority)
 {
     PriorityTable temp;
 
-    for (size_t i = 0; i < opSize; ++i)
+    for (size_t i = 0; i < opList.size(); ++i)
     {
-        temp.insert(std::make_pair(std::string(opList[i]), priority[i]));
+        temp.insert(std::make_pair(opList[i], priority[i]));
     }
 
     return temp;
 }
 
 const PriorityTable INPUT_PRIORITY =
-    InitPriorityTable(OPERATORS, OPERATORS_SIZE, INIT_INPUT_PRIORITY);
+    InitPriorityTable(OPERATORS, INIT_INPUT_PRIORITY);
 const PriorityTable STACK_PRIORITY =
-    InitPriorityTable(OPERATORS, OPERATORS_SIZE, INIT_STACK_PRIORITY);
+    InitPriorityTable(OPERATORS, INIT_STACK_PRIORITY);
 }  // namespace CubbyCalc
 
 #endif
